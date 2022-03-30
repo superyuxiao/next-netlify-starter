b1c91daee13666510829c960296f4fe1224aeb63
superyuxiao <41314611+superyuxiao@users.noreply.github.com>
2022-03-30 13:38:16 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="New Pages !" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hello, YuXiao <code>pages/index.js</code>
         </p>
       </main>
 
